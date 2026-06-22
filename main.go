fd382dd7dc7bab731a5f362a67cc88b3ca71c917
evaaiuse <evaaiuse@gmail.com>
2026-06-22 12:04:39 +0530

    Format code

--- main.go
+++ main.go
@@ -1,47 +1,47 @@
 package main
 
 import (
-"log"
-"os"
+	"log"
+	"os"
 
-tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func main() {
-token := os.Getenv("BOT_TOKEN")
+	token := os.Getenv("BOT_TOKEN")
 
-bot, err := tgbotapi.NewBotAPI(token)
-if err != nil {
-log.Fatal(err)
-}
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-log.Printf("Bot started: %s", bot.Self.UserName)
+	log.Printf("Bot started: %s", bot.Self.UserName)
 
-u := tgbotapi.NewUpdate(0)
-u.Timeout = 60
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
 
-updates := bot.GetUpdatesChan(u)
+	updates := bot.GetUpdatesChan(u)
 
-for update := range updates {
-if update.Message == nil {
-continue
-}
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
 
-switch update.Message.Text {
+		switch update.Message.Text {
 
-case "/start":
-msg := tgbotapi.NewMessage(
-update.Message.Chat.ID,
-"Hello! I'm online ✅",
-)
-bot.Send(msg)
+		case "/start":
+			msg := tgbotapi.NewMessage(
+				update.Message.Chat.ID,
+				"Hello! I'm online ✅",
+			)
+			bot.Send(msg)
 
-case "/ping":
-msg := tgbotapi.NewMessage(
-update.Message.Chat.ID,
-"Pong 🏓",
-)
-bot.Send(msg)
-}
-}
+		case "/ping":
+			msg := tgbotapi.NewMessage(
+				update.Message.Chat.ID,
+				"Pong 🏓",
+			)
+			bot.Send(msg)
+		}
+	}
 }
